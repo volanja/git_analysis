70876364dede982351dd3a781bee20fe40dba432
volanja <wandervogel.erde@gmail.com>
2014-07-20 17:46:50 +0900

    git logの一覧を取得できるまで出来た

--- lib/git_analysis.rb
+++ lib/git_analysis.rb
@@ -1,6 +1,7 @@
 require "git_analysis/version"
 require "git"
 require "fileutils"
+require "pp"
 
 module GitAnalysis
 
@@ -10,5 +11,23 @@ module GitAnalysis
       puts 'Directory Not Found. (./.git/stats)'
       exit 1
     end
+    g = Git.open(Dir::pwd)
+    p g.config('remote.origin.url')
+    # Get Commit Infomation
+    list = Hash.new
+    g.log.each {|l|
+      log = Hash.new
+      log["sha"] = l.sha
+      log["date"] = l.author.date
+      log["email"] = l.author.email
+      log["name"] = l.author.name
+      log["mesagge"] = l.message
+
+      list["#{l.sha}"] = log
+    }
+    pp list
   end
+
+  # 以降はプライベートメソッド
+  private
 end
